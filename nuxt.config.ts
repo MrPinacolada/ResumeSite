2383d5898722175df32b17249cfdc990c311da62
MrPinacolada <vasiasko112@gmail.com>
2024-10-08 17:41:10 +0300

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
 
   components: [
     {
@@ -11,5 +11,6 @@ export default defineNuxtConfig({
   ],
   css: ["~/assets/styles/index.scss"],
   modules: ["nuxt-icons"],
+  
   ssr: false,
 });
