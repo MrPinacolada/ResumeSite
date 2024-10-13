55de7bdb711bb2266adfba650ea217302300849e
MrPinacolada <vasiasko112@gmail.com>
2024-10-13 13:30:54 +0300

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
   app: {
+    baseURL: "/ResumeSite/",
     pageTransition: { name: "slide-fade", mode: "out-in" },
   },
   components: [
