4a1239bcf3231d860e880e34ad0e79f005536fef
MrPinacolada <vasiasko112@gmail.com>
2025-02-11 23:01:49 +0300

    update lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,20 @@
+import eslintPluginVue from "eslint-plugin-vue";
+
 export default [
-    {
-      ignores: ["node_modules", "dist"]
+  {
+    ignores: ["node_modules", "dist"]
+  },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
     },
-    {
-      languageOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module"
-      },
-      files: ["**/*.ts", "**/*.tsx", "**/*.vue"],
-      plugins: {
-        vue: require("eslint-plugin-vue")
-      },
-      rules: {
-        "vue/multi-word-component-names": "off"
-      }
+    files: ["**/*.ts", "**/*.tsx", "**/*.vue"],
+    plugins: {
+      vue: eslintPluginVue
+    },
+    rules: {
+      "vue/multi-word-component-names": "off"
     }
-  ];
-  
+  }
+];
